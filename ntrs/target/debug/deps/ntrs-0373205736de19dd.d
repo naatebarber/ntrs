4ff2be98541d5c6b1fe7a2331d05d46fcee3e333
nathanbarber6@gmail.com <nathanbarber6@gmail.com>
2023-12-17 12:40:52 -0500

    init

---
+++ ntrs/target/debug/deps/ntrs-0373205736de19dd.d
@@ -0,0 +1,5 @@
+/home/nathan/Desktop/ntrs/ntrs/target/debug/deps/libntrs-0373205736de19dd.rmeta: src/main.rs
+
+/home/nathan/Desktop/ntrs/ntrs/target/debug/deps/ntrs-0373205736de19dd.d: src/main.rs
+
+src/main.rs:
---
+++ ntrs/target/debug/deps/ntrs-d6efd62aa2bf804f.d
@@ -0,0 +1,5 @@
+/home/nathan/Desktop/ntrs/ntrs/target/debug/deps/libntrs-d6efd62aa2bf804f.rmeta: src/main.rs
+
+/home/nathan/Desktop/ntrs/ntrs/target/debug/deps/ntrs-d6efd62aa2bf804f.d: src/main.rs
+
+src/main.rs:
